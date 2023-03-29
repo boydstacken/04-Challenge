2785b715edd3215bfd292540619b73fef9ca71a1
boydstacken <124229670+boydstacken@users.noreply.github.com>
2023-03-28 21:58:21 -0500

    start of javascript

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+
+var answer1 = document.getElementsByClassName(booleans)
+var answer2 = document.getElementsByClassName(parenthesis)
+var answer3 = document.getElementsByClassName(all-of-the-above)
+var anwser4 = document.getElementsByClassName(curly_brackets)
+var answer5 = document.getElementsByClassName(forloops)
+
+
