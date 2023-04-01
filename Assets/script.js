c43a9c84e6d25dbc864d62cd301aeb98ab2894e9
boydstacken <124229670+boydstacken@users.noreply.github.com>
2023-04-01 09:03:31 -0500

    another push

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,36 @@
 
-var answer1 = document.getElementsByClassName(booleans)
-var answer2 = document.getElementsByClassName(parenthesis)
-var answer3 = document.getElementsByClassName(all-of-the-above)
-var anwser4 = document.getElementsByClassName(curly_brackets)
-var answer5 = document.getElementsByClassName(forloops)
+
+var startbutton = document.querySelector(".Start-Quiz")
+var question1 = document.querySelector(".Question-1")
+var question2 = document.querySelector(".Question-2")
+var question3 = document.querySelector(".Question-3")
+var question4 = document.querySelector(".Question-4")
+var question5 = document.querySelector(".Question-5")
+//var answer1 = document.querySelector(".Ordered-list-button-1")
+
+
+
+startbutton.addEventListener("click", startgame)
+
+function startgame(){
+    startbutton.classList.add("hide")
+    question1.parentNode.classList.remove("hide")
+    question1.parentNode.classList.add("show")
+}
+//for (var i = 0; i < orderedListButtons1.length; i++) {
+   //orderedListButtons1[i].addEventListener("click", answer1)
+//}
+
+//question1.addEventListener("click", orderedListButton1)
+
+//function orderedListButton1() {
+   // orderedListButton1.classList.add("hide")
+    //question2.parentNode.classList.remove("hide")
+    //question2.parentNode.classList.add("show")
+//}
+
+//for (var i = 0; i < orderedListButtons1.length; i++) {
+   // orderedListButtons1[i].addEventListener()
+ //}
 
 
