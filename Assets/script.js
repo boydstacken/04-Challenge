6a2fd97e7cb68167398913b237b4654abead0bea
boydstacken <124229670+boydstacken@users.noreply.github.com>
2023-04-02 12:52:52 -0500

    eliminated some extra code

--- Assets/script.js
+++ Assets/script.js
@@ -6,13 +6,12 @@ var question2 = document.querySelector(".Question-2")
 var question3 = document.querySelector(".Question-3")
 var question4 = document.querySelector(".Question-4")
 var question5 = document.querySelector(".Question-5")
-//var answer1 = document.querySelector(".Ordered-list-button-1")
 var answer1 = ["strings", "booleans","alerts", "numbers"]
 var answer2 = ["quotes","curly brackets", "parenthesis", "square brackets"]
 var answer3 = ["numbers and strings", "other arrays", "booleans", "all of the above"]
 var answer4 = ["commas", "curly brackets", "quotes","paranthesis"]
 var answer5 = ["JavaScript", "terminal/bash", "for loops"]
-correctanswer = ["booleans","paranthesis", "all of the above", "curly brackets","for loops"]
+var correctanswer = ["booleans","paranthesis", "all of the above", "curly brackets","for loops"]
 
 
 startbutton.addEventListener("click", startgame)
@@ -22,23 +21,23 @@ function startgame(){
     question1.parentNode.classList.remove("hide")
     question1.parentNode.classList.add("show")
 }
+ 
 
-answerbutton.addEventListener("click", startgame)
+answer1.addEventListener("click", correctanswer)
+    console.log(event.target); 
 
-for (var i = 0; i < orderedListButtons1.length; i++) {
+for (var i = 0; i < answerChoices1.length; i++) {
+    orderedListButtons1[i].addEventListener("click", answer1)
+ }
+
+
+
+
+
+//for (var i = 0; i < orderedListButtons1.length; i++) {
    orderedListButtons1[i].addEventListener("click", answer1)
-}
-//if 
-   // answer1 !==="strings"
-    //startbutton.classList.add("hide")
-    //question1.parentNode.classList.remove("hide")
-   // question1.parentNode.classList.add("show")
-//{
-//} else {
-    answer1 === "strings"
-    startbutton.classList.add("hide")
-    question1.parentNode.classList.remove("hide")
-    question1.parentNode.classList.add("show")
+//}
+
 //}
 
 //question1.addEventListener("click", orderedListButton1)
