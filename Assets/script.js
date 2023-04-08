b52672a0acb2a69c41f56e601ae10901b83bd8e1
boydstacken <124229670+boydstacken@users.noreply.github.com>
2023-04-08 11:13:11 -0500

    got timer start on button click by installing my setinterval code into the necessary function

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,5 @@
+
+
 var startbutton = document.querySelector(".Start-Quiz")
 var question1 = document.querySelector(".Question-1-choices");
 var question2 = document.querySelector(".Question-2")
@@ -13,6 +15,9 @@ var correctanswer = ["booleans", "paranthesis", "all of the above", "curly brack
 var choiceEl = document.querySelectorAll('.Ordered-list-button-1');
 var timer;
 var ele = document.getElementById('timer')
+
+
+
 // let questions  = [
 //     {
 //         question:  "questions1",
@@ -25,38 +30,51 @@ var ele = document.getElementById('timer')
 //         corrAns: "a3",
 //     },
 // ]
+
 // let questions = [question1, question2, question3, question4, question5];
 // let answers = [answer1, answer2, answer3, answer4, ]
 // let currentIndex =0;
 // let currentQuestion= questions[currentIndex];
+
 let currentQuestion = question1;
 let currentIndex = 0;
+
+
 startbutton.addEventListener("click", startgame)
+
+
+
 function startgame() {
     startbutton.classList.add("hide")
     question1.parentNode.classList.remove("hide")
     question1.parentNode.classList.add("show")
-    // setTimeout(downloadTimer,1000);
+    handleQuestion();
     var timeleft = 75;
 var downloadTimer = setInterval(function(){
-    setTimeout(downloadTimer,1000); 
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    document.getElementById("timer").innerHTML = "Finished";
-  } else {
-    document.getElementById("timer").innerHTML = timeleft + " seconds remaining";
-  }
-  timeleft -= 1;
-}, 1000);
-    handleQuestion();
+        setTimeout(downloadTimer,1000);  
+      if(timeleft <= 0){
+        clearInterval(downloadTimer);
+        document.getElementById("timer").innerHTML = "Finished";
+      } else {
+        document.getElementById("timer").innerHTML = timeleft + " seconds remaining";
+      }
+      timeleft -= 1;
+    }, 1000);
+        handleQuestion();
 }
-setTimeout(() => {  
-    startbutton.addEventListener("click", startgame)
-    console.log("this is the first message");
-      }, 5000);
+
+
+//setTimeout(() => {   
+    //startbutton.addEventListener("click", startgame)
+    //console.log("this is the first message");
+     // }, 5000);
+
+
+
 function endquiz() {
     // to be completed
 }
+
 clickEvent = (event) => {
     console.log('some event content here...')
     let btn = event.target;
@@ -68,6 +86,7 @@ clickEvent = (event) => {
         // tbd , penalise by docking 10 seconds off of timer
         alert(" Oops, that  is not  right");
     }
+
     currentIndex++;
     if (currentIndex === correctanswer.length) {
         // tbd end the quiz , because exhausted all questions
@@ -75,13 +94,15 @@ clickEvent = (event) => {
     } else {
         handleQuestion();
     }
+
 }
 // choiceEl.forEach((item) => {
 //     item.addEventListener('click', clickEvent)
 // });
+
 function handleQuestion() {
     currentQuestion.parentNode.classList.add("hide")
-    choiceEl = document.querySelectorAll(.Ordered-list-button-${currentIndex + 1});
+    choiceEl = document.querySelectorAll(`.Ordered-list-button-${currentIndex + 1}`);
     switch (currentIndex + 1) {
         case 1:
             currentQuestion = question1;
@@ -98,6 +119,7 @@ function handleQuestion() {
         case 5:
             currentQuestion = question5;
             break;
+
     }
     currentQuestion.parentNode.classList.remove("hide")
     currentQuestion.parentNode.classList.add("show")
@@ -108,22 +130,29 @@ function handleQuestion() {
         item.addEventListener('click', clickEvent)
     });
 }
+
+
+
+
+
+
+
 //for (var i = 0; i < orderedListButtons1.length; i++) {
-
-
-
-
-
-
 //    orderedListButtons1[i].addEventListener("click", answer1)
 //}
+
 //}
+
 //question1.addEventListener("click", orderedListButton1)
+
 //function orderedListButton1() {
    // orderedListButton1.classList.add("hide")
     //question2.parentNode.classList.remove("hide")
     //question2.parentNode.classList.add("show")
 //}
+
 //for (var i = 0; i < orderedListButtons1.length; i++) {
    //orderedListButtons1[i].addEventListener()
-// }```
+// }
+
+
