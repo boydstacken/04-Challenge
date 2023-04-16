0a2ee16acebbe6d42fe889f8769dd5dc8f50f3a7
boydstacken <124229670+boydstacken@users.noreply.github.com>
2023-04-16 10:22:37 -0500

    stuck on quiz going to scores page

--- Assets/script.js
+++ Assets/script.js
@@ -51,7 +51,7 @@ var downloadTimer = setInterval(function(){
 // when user completes quiz
 //function endquiz() {
     //scorepage.textContent.add.("show")
-}
+//}
 
 //events when user clicks button and alerts saying if their answer is right or wrong. Time is deducted by -10 seconds if they select the wrong answer
 clickEvent = (event) => {
@@ -112,4 +112,39 @@ function checkAnswer(){
     console.log(answer)
     score = score + 5;
     alert(score)
+}
+var questions = [
+    {
+      title: 'Commonly used data types DO NOT include:',
+      choices: ['strings', 'booleans', 'alerts', 'numbers'],
+      answer: 'alerts',
+    },
+    {
+      title: 'The condition in an if / else statement is enclosed within ____.',
+      choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+      answer: 'parentheses',
+    },
+    {
+        title: 'Arrays in JavaScript can be used to store _____.',
+        choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+        answer: 'all of the above',
+    },
+    {
+        title: 'String values must be enclosed within _____ when being assigned to variables.',
+        choices: ['commas', 'curly brackets', 'quotes', 'paranthesis'],
+        answer: 'curly brackets',
+    },
+    {
+        title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        choices: ['Javascript', 'terminal/bash', 'for loops', 'console log'],
+        answer: 'for loops',
+    },
+]
+//scorebutton.addEventListener("click", seescores)
+
+//function seescores() {
+    //var scorebutton = document.querySelector(".Score-Button")
+    //scorebutton.classList.add("hide")
+   // scorepage.classList.remove("show")
+    //score.classList.add("show")
 }
